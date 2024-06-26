6096389b9b2f6f784bca1b40ced9589297cb1af0
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-06-26 16:56:25 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let c =  String(prompt('string tyaptigi malumot kiriting'))
+let b = Number(prompt('number tyaptigi malumot kiriting'))
+let a = Boolean(prompt('boolean tyaptigi malumot kiriting'))
+
+
+
+alert('hamma malumotlarni kiritganizga ishochnigiz komilmi')
+alert ('siz kiritgan  sozi string typidan olingan ' + c +  ('\nsiz kiritgan  sozi number typidan olingan ' +b)  +  ('\nsiz kiritgan  sozi boolean typidan olingan ' +a)  )  
